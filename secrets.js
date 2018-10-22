7ad1ebe20d8a6a1b4ea54aae1426816f7f43db59
Rick Conrad <richardjconrad@gmail.com>
2018-10-22 11:55:25 +0100

    Remove secrets file again

--- secrets.js
+++
@@ -1,7 +0,0 @@
-module.exports =
-  process.env.NODE_ENV == "production"
-    ? {
-        dbPassword: process.env.BHA_DB_PASSWORD,
-        apiKey: process.env.BHA_KEY
-      }
-    : require("./devSecrets.js");
